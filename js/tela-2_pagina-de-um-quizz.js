4753e73f71649f3915efb8ac1328dd1484caa0a5
Vanessa Furquim <vanessafurquim.contato@gmail.com>
2022-02-21 02:07:50 -0300

    fixing bugs in showing chosen quizz on screen, implementing function for applying opacity to answers (bugged), implementing randomized answer order (bugged)

--- js/tela-2_pagina-de-um-quizz.js
+++ js/tela-2_pagina-de-um-quizz.js
@@ -10,67 +10,109 @@ function showSelectedQuizzOnScreen(selectedQuizzContent) {
     addBackgroundImage.innerHTML += `
     <img src="${selectedQuizzContent.data.image}">
     <h3>${selectedQuizzContent.data.title}</h3>`
-    
+
     let addQuestions = document.querySelector(".question-container");
     addQuestions.innerHTML = "";
 
-    if(selectedQuizzContent.data.questions[0].answers.length === 4){
-        for(let i = 0; i < selectedQuizzContent.data.questions.length; i++){
+    if(selectedQuizzContent.data.questions[0].answers.length === 4) {
+        for(let i = 0; i < selectedQuizzContent.data.questions.length; i++) {
             addQuestions.innerHTML += `
-            <span style="background-color: ${selectedQuizzContent.data.questions[i].color}">${selectedQuizzContent.data.questions[i].title}</span>
+            <div class="question-title" style="background-color: ${selectedQuizzContent.data.questions[i].color}">
+            <span>${selectedQuizzContent.data.questions[i].title}</span>
+            </div>
             <div class = "question-box">
-                <figure class="image-1 ${selectedQuizzContent.data.questions[i].answers[0].isCorrectAnswer}" onclick="chosenAnswer(this)">
+                <figure class="answerImage ${selectedQuizzContent.data.questions[i].answers[0].isCorrectAnswer}" onclick="chosenAnswer(this)">
                     <img src="${selectedQuizzContent.data.questions[i].answers[0].image}">
-                    <figcaption>${selectedQuizzContent.data.questions[i].answers[0].text}</figcaption>
+                    <figcaption class = "selectedCaption">${selectedQuizzContent.data.questions[i].answers[0].text}</figcaption>
                 </figure>
-                <figure class="image-2  ${selectedQuizzContent.data.questions[i].answers[1].isCorrectAnswer}" onclick="chosenAnswer(this)">
-                    <img src="${selectedQuizzContent.data.questions[i].answers[1].image}">
-                    <figcaption>${selectedQuizzContent.data.questions[i].answers[1].text}</figcaption>
+                <figure class = "answerImage ${selectedQuizzContent.data.questions[i].answers[1].isCorrectAnswer}" onclick="chosenAnswer(this)">
+                    <img src = "${selectedQuizzContent.data.questions[i].answers[1].image}">
+                    <figcaption class = "selectedCaption">${selectedQuizzContent.data.questions[i].answers[1].text}</figcaption>
                 </figure>
-                <figure class="image-3 ${selectedQuizzContent.data.questions[i].answers[2].isCorrectAnswer}" onclick="chosenAnswer(this)">
+                <figure class="answerImage ${selectedQuizzContent.data.questions[i].answers[2].isCorrectAnswer}" onclick="chosenAnswer(this)">
                     <img src="${selectedQuizzContent.data.questions[i].answers[2].image}">
-                    <figcaption>${selectedQuizzContent.data.questions[i].answers[2].text}</figcaption>
+                    <figcaption class = "selectedCaption">${selectedQuizzContent.data.questions[i].answers[2].text}</figcaption>
                 </figure>
-                <figure class="image-4 ${selectedQuizzContent.data.questions[i].answers[3].isCorrectAnswer}" onclick="chosenAnswer(this)">
+                <figure class="answerImage ${selectedQuizzContent.data.questions[i].answers[3].isCorrectAnswer}" onclick="chosenAnswer(this)">
                     <img src="${selectedQuizzContent.data.questions[i].answers[3].image}">
-                    <figcaption>${selectedQuizzContent.data.questions[i].answers[3].text}</figcaption>
+                    <figcaption class = "selectedCaption">${selectedQuizzContent.data.questions[i].answers[3].text}</figcaption>
                 </figure>
             </div>`
+
+            //addQuestions = addQuestions.sort(() => Math.random() - 0.5);
             
         }
     } else if (selectedQuizzContent.data.questions[0].answers.length === 3){
         for(let i = 0; i < selectedQuizzContent.data.questions.length; i++){
-            addPerguntas.innerHTML += `
-            <span style="background-color: ${selectedQuizzContent.data.questions[i].color}">${selectedQuizzContent.data.questions[i].title}</span>
+            addQuestions.innerHTML += `
+            <div class="question-title" style="background-color: ${selectedQuizzContent.data.questions[i].color}">
+            <span>${selectedQuizzContent.data.questions[i].title}</span>
+            </div>
             <div class = "question-box">
-                <figure class="image-1 ${selectedQuizzContent.data.questions[i].answers[0].isCorrectAnswer}" onclick="chosenAnswer(this)">
+                <figure class="image ${selectedQuizzContent.data.questions[i].answers[0].isCorrectAnswer}" onclick="chosenAnswer(this)">
                     <img src="${selectedQuizzContent.data.questions[i].answers[0].image}">
-                    <figcaption>${selectedQuizzContent.data.questions[i].answers[0].text}</figcaption>
+                    <figcaption class = "selectedCaption">${selectedQuizzContent.data.questions[i].answers[0].text}</figcaption>
                 </figure>
-                <figure class="image-2  ${selectedQuizzContent.data.questions[i].answers[1].isCorrectAnswer}" onclick="chosenAnswer(this)">
+                <figure class="image ${selectedQuizzContent.data.questions[i].answers[1].isCorrectAnswer}" onclick="chosenAnswer(this)">
                     <img src="${selectedQuizzContent.data.questions[i].answers[1].image}">
-                    <figcaption>${selectedQuizzContent.data.questions[i].answers[1].text}</figcaption>
+                    <figcaption class = "selectedCaption">${selectedQuizzContent.data.questions[i].answers[1].text}</figcaption>
                 </figure>
-                <figure class="image-3 ${selectedQuizzContent.data.questions[i].answers[2].isCorrectAnswer}" onclick="chosenAnswer(this)">
+                <figure class="image ${selectedQuizzContent.data.questions[i].answers[2].isCorrectAnswer}" onclick="chosenAnswer(this)">
                     <img src="${selectedQuizzContent.data.questions[i].answers[2].image}">
-                    <figcaption>${selectedQuizzContent.data.questions[i].answers[2].text}</figcaption>
+                    <figcaption class = "selectedCaption">${selectedQuizzContent.data.questions[i].answers[2].text}</figcaption>
                 </figure>
             </div>`
+
+            //addQuestions = addQuestions.sort(() => Math.random() - 0.5);
+
         }
-    } else if (conteudo.data.questions[0].answers.length === 2){
-        for(let i = 0; i < conteudo.data.questions.length; i++){
-            addPerguntas.innerHTML += `
-            <span style="background-color: ${selectedQuizzContent.data.questions[i].color}">${selectedQuizzContent.data.questions[i].title}</span>
+    } else if (selectedQuizzContent.data.questions[0].answers.length === 2){
+        for(let i = 0; i < selectedQuizzContent.data.questions.length; i++){
+            addQuestions.innerHTML += `
+            <div class="question-title" style="background-color: ${selectedQuizzContent.data.questions[i].color}">
+            <span>${selectedQuizzContent.data.questions[i].title}</span>
+            </div>
             <div class = "question-box">
-                <figure class="image-1 ${selectedQuizzContent.data.questions[i].answers[0].isCorrectAnswer}" onclick="chosenAnswer(this)">
+                <figure class="image ${selectedQuizzContent.data.questions[i].answers[0].isCorrectAnswer}" onclick="chosenAnswer(this)">
                     <img src="${selectedQuizzContent.data.questions[i].answers[0].image}">
+                    <figcaption class = "selectedCaption">${selectedQuizzContent.data.questions[i].answers[2].text}</figcaption>
                     <figcaption>${selectedQuizzContent.data.questions[i].answers[0].text}</figcaption>
                 </figure>
-                <figure class="image-2  ${selectedQuizzContent.data.questions[i].answers[1].isCorrectAnswer}" onclick="chosenAnswer(this)">
+                <figure class="image ${selectedQuizzContent.data.questions[i].answers[1].isCorrectAnswer}" onclick="chosenAnswer(this)">
                     <img src="${selectedQuizzContent.data.questions[i].answers[1].image}">
+                    <figcaption class = "selectedCaption">${selectedQuizzContent.data.questions[i].answers[2].text}</figcaption>
                     <figcaption>${selectedQuizzContent.data.questions[i].answers[1].text}</figcaption>
                 </figure>
             </div>`
+
+            //addQuestions = addQuestions.sort(() => Math.random() - 0.5);
         }
     }
+}
+
+function chosenAnswer(answerOptions) {
+
+    let chosenAnswerOption = answerOptions.parentNode;
+    let selectingFigures = chosenAnswerOption.children;
+
+    for(let i = 0; i < selectingFigures.length; i++) {
+        
+        selectingFigures[i].firstElementChild.classList.add("whitened-image");
+        selectingFigures[i].setAttribute("onclick", "");
+
+        if(selectingFigures[i].classList.contains('true')) {
+            const imageCaption = selectingFigures[i].lastElementChild;
+            imageCaption.classList.add("green-text");
+        } else {
+            const imageCaption = selectingFigures[i].lastElementChild;
+            imageCaption.classList.add("red-text");
+        }
+    }
+
+    answerOptions.classList.remove("whitened-image");
+
+    setTimeout(function() {
+        answerOptions.scrollIntoView({behavior: 'smooth', block:'center'})
+    }, 2000);
+
 }
